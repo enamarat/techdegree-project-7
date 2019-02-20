94481170d4b3a47d06342a05daf01bb5f3cbfa20
Marat Enaliev <enamarat@yandex.ru>
2019-02-20 21:27:53 +0300

    Render home page template.

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
 const express = require('express');
-const jsonData = require('/data.json');
+const data = require('./data.json');
+const projects = data.projects;
 const app = express();
 
+
 app.set('view engine', 'pug');
-app.use('./static', express.static('public'));
+app.use('/static', express.static('public'));
 
 // Setting routes
 app.get('/', (request, response) => {
-  response.locals = data.projects;
-  response.render('index.pug');
+  // response.locals = data.projects;
+  response.render('index.pug', {projects: projects});
 });
 
 app.get('/about', (request, response) => {
   response.render('about.pug');
 });
 
-app.get('/projects', (request, response) => {
-  response.render('project.pug');
+app.get('/projects/:id', (request, response) => {
+  response.render('project.pug', {
+    id: request.param.id,
+    title: projects[id].project_name,
+    description: projects[id].description,
+    technologies: projects[id].technologies,
+    live_link: projects[id].live_link,
+    github_link: projects[id].github_link,
+    image_urls: data.projects[req.param.id].image_urls
+  });
 });
 
+// Listening to server
 app.listen(3000, () => {
-  console.log('The app is listening to port 3000!')
+  console.log('The app is listening to port 3000!');
+
 });
