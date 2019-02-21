6b4260618f46256ac8105dd8f74abb283db58a2d
Marat Enaliev <enamarat@yandex.ru>
2019-02-21 16:53:03 +0300

    Reneder templates of projects' pages.

--- app.js
+++ app.js
@@ -17,20 +17,30 @@ app.get('/about', (request, response) => {
   response.render('about.pug');
 });
 
-app.get('/projects/:id', (request, response) => {
+app.get('/project/:id', (request, response) => {
+  const {id} = request.params
   response.render('project.pug', {
-    id: request.param.id,
     title: projects[id].project_name,
     description: projects[id].description,
     technologies: projects[id].technologies,
     live_link: projects[id].live_link,
     github_link: projects[id].github_link,
-    image_urls: data.projects[req.param.id].image_urls
+    image_urls: data.projects[request.params.id].image_urls
   });
 });
+
 
 // Listening to server
 app.listen(3000, () => {
   console.log('The app is listening to port 3000!');
-
 });
+
+// {
+//   id: request.param.id,
+//   title: projects[id].project_name,
+//   description: projects[id].description,
+//   technologies: projects[id].technologies,
+//   live_link: projects[id].live_link,
+//   github_link: projects[id].github_link,
+//   image_urls: data.projects[request.param.id].image_urls
+// }
