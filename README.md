6b97d1d72576fb8a8cd832d71c105d38864269cf
Marat Enaliev <enamarat@yandex.ru>
2019-02-21 20:48:47 +0300

    Completion of the project.

--- README.md
+++ README.md
@@ -2,3 +2,21 @@
 
 
 A portfolio site which is built using Node.js, Express and Pug.
+
+Changes in styles:
+background-color
+1.  before: .portfolio-me { bacground: #121213}
+	 now: .portfolio-me {bacground: #17d330}
+2. before: .portfolio-intro {background-color: none}
+	now: .portfolio-intro {background-color: #0fff77}
+color
+3. before: .portfolio-me p {color: #999}
+	now: .portfolio-me p {color: #110059}
+4. before: .portfolio-me a:hover {color: #999}
+	now: .portfolio-me a:hover {color: #e00808}
+box-shadow
+5. before: none
+	now: .portfolio-index .cell { box-shadow: 3px 3px 5px 5px rgba(0, 0, 0, 0.8)}
+font
+6. before: h1,h2,h3,h4,h5,p {font-family: 'Merryweather', serif}
+	now: h1,h2,h3,h4,h5,p {font-family: 'Helvetica', serif}
--- app.js
+++ app.js
@@ -3,20 +3,24 @@ const data = require('./data.json');
 const projects = data.projects;
 const app = express();
 
-
+// Setting template language
 app.set('view engine', 'pug');
+
+// Setting directory where ststic files are kept
 app.use('/static', express.static('public'));
 
 // Setting routes
+// Main page
 app.get('/', (request, response) => {
-  // response.locals = data.projects;
   response.render('index.pug', {projects: projects});
 });
 
+// Page "About Me"
 app.get('/about', (request, response) => {
   response.render('about.pug');
 });
 
+// Dynamically generated pages for each project
 app.get('/project/:id', (request, response) => {
   const {id} = request.params
   response.render('project.pug', {
@@ -29,18 +33,26 @@ app.get('/project/:id', (request, response) => {
   });
 });
 
+// Handling errors
+// Handling 404 error
+app.use((request, response, next) => {
+  const error = new Error('Page not found');
+  error.status = 404;
+  next(error);
+})
+
+// Error handler
+app.use((error, request, response, next) => {
+  response.locals.error = error;
+  response.status(error.status);
+  console.log(`${error}. Status code of the error: ${error.status}`);
+  response.render('error.pug', {
+    error: error,
+    errorStatus: error.status
+  })
+})
 
 // Listening to server
 app.listen(3000, () => {
   console.log('The app is listening to port 3000!');
 });
-
-// {
-//   id: request.param.id,
-//   title: projects[id].project_name,
-//   description: projects[id].description,
-//   technologies: projects[id].technologies,
-//   live_link: projects[id].live_link,
-//   github_link: projects[id].github_link,
-//   image_urls: data.projects[request.param.id].image_urls
-// }
